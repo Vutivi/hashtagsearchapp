05b75364d686f622d3c2ac92e06049f7730f213d
vutivi <201141790@student.uj.ac.za>
2019-02-01 11:52:56 +0200

    chore: add instagram service

---
+++ app/services/hashtag_services.rb
@@ -0,0 +1,22 @@
+class HashtagService
+  FACEBOOK_USER_ID = 100008491682897
+
+  def self.search_hashtag_posts search_id
+    request = HTTParty.get("graph.facebook.com/#{search_id}/recent_media?\
+      user_id=#{FACEBOOK_USER_ID}")
+
+    JSON.parse(response.body)["data"]
+  end
+
+  def self.search_hashtag search_term
+    request = HTTParty.get("graph.facebook.com/ig_hashtag_search?\
+      user_id=#{FACEBOOK_USER_ID}&q=coke")
+
+    response.body["id"]
+  end
+
+  def self.get_post_with_hashtag post_id
+    request = HTTParty.get("https://graph.facebook.com/v3.2/posts/#{post_id}")
+    JSON.parse(response.body)["data"]
+  end
+end
