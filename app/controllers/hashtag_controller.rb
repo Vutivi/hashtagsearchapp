f3dbf55cdd43f411ac0fe470aec761053dccf0c3
vutivi <201141790@student.uj.ac.za>
2019-02-01 13:28:31 +0200

    fix: wrong search key

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -1,11 +1,13 @@
 class HashtagController < ApplicationController
   def index
-    @hashtags = []
+    @hashtag_posts = []
   end
 
   def mark
   end
 
   def search
+    hashtag_id     = HashtagService.search_hashtag(params[:search_term])
+    @hashtag_posts = HashtagService.search_hashtag_posts(hashtag_id)
   end
 end
